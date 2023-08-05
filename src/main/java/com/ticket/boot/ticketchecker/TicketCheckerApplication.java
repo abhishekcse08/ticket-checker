8853d849751175a745f6f0b30880b487455f46f3
abhishekcse08 <abhishekcse08@gmail.com>
2023-08-05 07:24:02 +0530

    Initial commit

---
+++ src/main/java/com/ticket/boot/ticketchecker/TicketCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.boot.ticketchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/boot/ticketchecker/TicketCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.boot.ticketchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
